c5fad0f047dd3ecee01c834979ad10e4fbbeb41c
Eliot Langlois <75205183+wirare@users.noreply.github.com>
2026-06-30 17:41:19 +0200

    Update cred.py
    fix credential

--- ftl_quantum/cred.py
+++ ftl_quantum/cred.py
@@ -1,8 +1,8 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
 
 QiskitRuntimeService.save_account(
-    token="kqy3gPPuZoEl1CUWZCc7HN4h0bckPQsHL9hbyTJ-cmyl",
-    instance="crn:v1:bluemix:public:quantum-computing:us-east:a/9bd39dede856445ba185998f84a136ed:5f7227ee-8200-40a9-826a-f8ef7be8c744::",
+    token="IMB-API-TOKEN",
+    instance="INSTANCE-TOKEN",
     set_as_default=True,
     overwrite=True,
 )
